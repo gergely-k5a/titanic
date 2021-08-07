82d5ac276daf38cb9306389c99ced0551602319c
Gergely K5a <dageekymongrel@pm.me>
2021-08-07 12:38:22 +0200

    prettier config

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,21 @@
-const router = require("express").Router();
-const fs = require("fs");
+const router = require('express').Router();
+const fs = require('fs');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.json({
-    msg: "Titanic API works.",
+    msg: 'Titanic API works.',
   });
 });
 
-router.get("/titanic", (req, res) => {
-  fs.readFile("data/titanic.json", (err, data) => {
+router.get('/titanic', (req, res) => {
+  fs.readFile('data/titanic.json', (err, data) => {
     // TODO: handle errors
     const result = JSON.parse(data);
     res.send(result);
   });
 });
 
-router.get("/label/:type", (req, res) => {
+router.get('/label/:type', (req, res) => {
   fs.readFile(`data/label_${req.params.type}.json`, (err, data) => {
     // TODO: handle errors
     const result = JSON.parse(data);
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
-const api = require("./routes/api");
+const api = require('./routes/api');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("/api", api);
+app.use('/api', api);
 
 app.listen(port, () => {
   console.log(`Titanic API listening at port ${port}`);

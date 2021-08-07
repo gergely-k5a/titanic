c7eee371fdd45d7cfcd1bac7f03a4d3c836d75c0
Gergely K5a <dageekymongrel@pm.me>
2021-08-07 13:42:49 +0200

    fetch passenger data; display it using ant design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
+import Layout, { Content, Footer, Header } from 'antd/lib/layout/layout';
 
 function App() {
+  const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios('/api/titanic').then((res) => {
+      const { result } = res.data;
+
+      const cols = result.attributes.map((attr) => ({
+        title: attr.name,
+        dataIndex: attr.name,
+        key: attr.name.toLowerCase(),
+      }));
+
+      const dataSource = result.rows.map((row, key) => ({ key, ...row }));
+
+      setColumns(cols);
+      setRows(dataSource);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>
+        <h1>
+          <a href="\">Titanic</a>
+        </h1>
+      </Header>
+      <Content>
+        <Table dataSource={rows} columns={columns} />
+      </Content>
+      <Footer>
+        <h5>Created by Gergely K5a</h5>
+      </Footer>
+    </Layout>
   );
 }
 
